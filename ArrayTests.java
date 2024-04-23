ca08990e139cf2546a1f27dc2a553532835b4427
MaritessSamson <m1samson@ucsd.edu>
2024-04-23 15:52:55 -0700

    Some concise message describing the change

--- ArrayTests.java
+++ ArrayTests.java
@@ -22,4 +22,28 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testAverageWithoutLowestEmpty(){
+    double[] input1 = { };
+    assertEquals(Double.valueOf(0.0), Double.valueOf(ArrayExamples.averageWithoutLowest(input1)));
+  }
+
+  @Test
+  public void testAverageWithoutLowestOne(){
+    double[] input1 = { 2.0 };
+    assertEquals(Double.valueOf(0.0), Double.valueOf(ArrayExamples.averageWithoutLowest(input1)));
+  }
+
+  @Test
+  public void testAverageWithoutLowestRepeat(){
+    double[] input1 = { 1.0, 2.5, 1.0, 4.0, 1.5 };
+    assertEquals(Double.valueOf(2.0), Double.valueOf(ArrayExamples.averageWithoutLowest(input1)));
+  }
+
+  @Test
+  public void testAverageWithoutLowestNorm(){
+    double [] input1 = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    assertEquals(Double.valueOf(4.0), Double.valueOf(ArrayExamples.averageWithoutLowest(input1)));
+  }
 }
